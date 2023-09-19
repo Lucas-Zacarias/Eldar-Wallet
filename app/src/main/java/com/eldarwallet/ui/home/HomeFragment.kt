39fa204d3187a524e2d9e607d5dced517906daf2
Lucas-Zacarias <lucas21111999@gmail.com>
2023-09-19 10:08:05 -0300

    home screen migrated to compose

--- app/src/main/java/com/eldarwallet/ui/home/HomeFragment.kt
+++ app/src/main/java/com/eldarwallet/ui/home/HomeFragment.kt
@@ -1,35 +1,25 @@
 package com.eldarwallet.ui.home
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.eldarwallet.R
-import com.eldarwallet.databinding.FragmentHomeBinding
-import com.eldarwallet.domain.models.Card
 import com.eldarwallet.domain.usecases.HomeUseCase
-import com.eldarwallet.ui.adapter.CardsAdapter
 import com.eldarwallet.ui.login.LogInActivity
 import com.eldarwallet.ui.paywithcard.PayWithCardActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.eldarwallet.ui.screens.HomeScreen
+import com.eldarwallet.ui.theme.EldarWalletTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var userInitials: TextView
-    private lateinit var userGreeting: TextView
     private val homeViewModel: HomeViewModel by viewModels()
-    private lateinit var cardRV: RecyclerView
 
     @Inject
     lateinit var homeUseCase: HomeUseCase
@@ -39,51 +29,31 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(requireActivity()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                EldarWalletTheme {
+                    HomeScreen(
+                        userInitials = homeUseCase.getUserInitials(),
+                        userName = homeUseCase.getUserName(),
+                        viewModel = homeViewModel,
+                        signOffEvent = {
+                            logOut()
+                        },
+                        goToPayEvent = {
+                            goToPay(it)
+                        }
+                    )
+                }
+            }
+        }
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getViews()
-        setViews()
-        setObservables()
-        setListeners()
-    }
-
-    private fun getViews() {
-        userInitials = binding.tvUserInitials
-        userGreeting = binding.tvUserGreeting
-    }
-
-    private fun setViews() {
-        setUserInitials()
-        setUserGreeting()
-    }
-
-    private fun setObservables() {
         homeViewModel.getCardList()
-
-        homeViewModel.cardList.observe(viewLifecycleOwner){
-            if(it.isNotEmpty()) {
-                setUpRecyclerCards(it)
-                hideNoCards()
-                showRVCards()
-            }else{
-                showNoCards()
-            }
-        }
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    private fun setUpRecyclerCards(cards: List<Card>) {
-        cardRV = binding.rvCards
-        cardRV.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-
-        val adapter = CardsAdapter(cards){cardNumber -> goToPay(cardNumber)}
-        cardRV.adapter = adapter
-        adapter.notifyDataSetChanged()
     }
 
     private fun goToPay(cardNumber: ByteArray) {
@@ -93,54 +63,10 @@ class HomeFragment : Fragment() {
         startActivity(intent)
     }
 
-
-    private fun setUserInitials() {
-        userInitials.text = homeUseCase.getUserInitials()
-    }
-
-    private fun setUserGreeting() {
-        userGreeting.text =
-            getString(R.string.hello).plus(homeUseCase.getUserName() ?: "Eldar").plus("!")
-    }
-
-    private fun setListeners() {
-        openWindowToLogOut()
-    }
-
-    private fun openWindowToLogOut() {
-        userInitials.setOnClickListener {
-            showAlertLogOff()
-        }
-    }
-
-    private fun showAlertLogOff() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle("Cerrar Sesión")
-            .setMessage("¿Está seguro que desea cerrar sesión?")
-            .setPositiveButton("Si") { _, _ ->
-                logOut()
-            }
-            .setNegativeButton("Cancelar") { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
-    }
-
     private fun logOut() {
         homeUseCase.logOut()
         startActivity(Intent(activity, LogInActivity::class.java))
         requireActivity().finish()
     }
 
-    private fun hideNoCards() {
-        binding.clNoCardsAdded.visibility = View.GONE
-    }
-
-    private fun showNoCards() {
-        binding.clNoCardsAdded.visibility = View.VISIBLE
-    }
-
-    private fun showRVCards() {
-        binding.rvCards.visibility = View.VISIBLE
-    }
 }
---
+++ app/src/main/java/com/eldarwallet/ui/screens/reusablecomponents/CardItem.kt
@@ -0,0 +1,99 @@
+package com.eldarwallet.ui.screens.reusablecomponents
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eldarwallet.R
+import com.eldarwallet.ui.theme.EldarWalletTheme
+
+@Composable
+fun ReusableCardItem(
+    cardImage: Int,
+    cardHidedNumber: String,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier =
+        Modifier
+            .wrapContentSize()
+            .clickable {
+                onClick()
+            },
+        shape = RoundedCornerShape(10.dp),
+        colors =
+        CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        elevation =
+        CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter =
+                painterResource(cardImage),
+                contentDescription =
+                stringResource(R.string.card_image),
+                modifier =
+                Modifier.size(width = 200.dp, height = 150.dp)
+            )
+
+            Text(
+                text = cardHidedNumber,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 5.dp)
+            )
+        }
+
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "Light mode"
+)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Night mode"
+)
+@Composable
+fun PreviewCardItem() {
+    EldarWalletTheme {
+        Surface {
+            ReusableCardItem(
+                R.drawable.visa,
+                stringResource(id = R.string.example_hided_card_number),
+                onClick = {})
+        }
+    }
+}
