9986a6daf426574b75e687339d6fbb29ed37febb
Lucas-Zacarias <lucas21111999@gmail.com>
2023-10-23 10:21:43 -0300

    change logic to show cards added, cards will show order by their type

--- app/src/main/java/com/wallet/domain/usecases/HomeUseCase.kt
+++ app/src/main/java/com/wallet/domain/usecases/HomeUseCase.kt
@@ -2,6 +2,7 @@ package com.wallet.domain.usecases
 
 import android.content.SharedPreferences
 import com.wallet.domain.models.Card
+import com.wallet.domain.models.CardType
 import com.wallet.domain.services.RoomDatabaseService
 import javax.inject.Inject
 
@@ -13,7 +14,7 @@ class HomeUseCase @Inject constructor(
     private val editor = sharedPreferences.edit()
 
     suspend fun getCardList(): List<Card> {
-        return roomDatabaseService.getAllUserCards(getUserId())
+        return sortCardListByType(roomDatabaseService.getAllUserCards(getUserId()))
     }
 
     fun getUserInitials(): String {
@@ -23,7 +24,7 @@ class HomeUseCase @Inject constructor(
         return if (name != null && surname != null) {
             (name.first().plus(surname.first().toString())).uppercase()
         } else {
-            "EW"
+            "WL"
         }
     }
 
@@ -49,4 +50,25 @@ class HomeUseCase @Inject constructor(
         return 0
     }
 
+    private fun sortCardListByType(cards: List<Card>): List<Card> {
+        return cards.sortedWith(
+            compareBy{ card ->
+                getCardTypePriority(card.type)
+            }
+        )
+    }
+
+    private fun getCardTypePriority(cardType: ByteArray): Int {
+
+        return when(EncryptionUseCase.decrypt(cardType)) {
+            CardType.VISA.toString() -> 1
+
+            CardType.MASTERCARD.toString() -> 2
+
+            CardType.AMERICAN_EXPRESS.toString() -> 3
+
+            else -> 4
+         }
+
+    }
 }
